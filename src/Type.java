3e62bfdbc6eebd934f284707490e75a7c15a67aa
Najib <najib.aghenda@telecomnancy.eu>
2018-04-19 15:58:58 +0200

    ajout type

--- src/Type.java
+++ src/Type.java
@@ -1,4 +1,42 @@
-
 public class Type {
+	
+	String type;
+	private int vecSize;
+	private String structureType;
+	
+	public Type(String type) {
+		this.type = type;
+	}
+	
+	public Type(String type, int vecSize) {
+		this.type = type;
+		this.vecSize = vecSize;
+	}
+	
+	public boolean isVec() {
+		return this.vecSize > 0;
+	}
+	
+	public boolean isStruct() {
+		return this.type == "struct"; 
+	}
+	
+	public int getVecSize() {
+		return this.vecSize;
+	}
+	
+	public String getStructureType() {
+		return this.structureType;
+	}
+
+	
+	@Override
+	public boolean equals(Object object) {
+	    if(object instanceof Type) {
+	        return true;
+	    } else {
+	        return false;
+	    }
+	}
 
 }
