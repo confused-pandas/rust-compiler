f856a52d68873460f7d20053c44b83f6a58787ce
Najib Aghenda <najib.aghenda@gmail.com>
2018-04-27 15:25:07 +0200

    Le commit : partie I

--- src/exception/IfWithoutBoolException.java
+++ src/exception/IfWithoutBoolException.java
@@ -1,4 +1,11 @@
 package exception;
 
+import org.antlr.runtime.tree.Tree;
+
 public class IfWithoutBoolException extends SemanticException{
+	
+	public IfWithoutBoolException(String message) {
+		super(message);
+	}
+	
 }
