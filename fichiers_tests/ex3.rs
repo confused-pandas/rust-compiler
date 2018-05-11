466fdb4a099416574002e6f40b85f33a94cab32f
Damien VANTOUROUT <damien.vantourout@telecomnancy.eu>
2018-05-11 09:13:40 +0200

    Restoration ex3.rs

--- fichiers_tests/ex3.rs
+++ fichiers_tests/ex3.rs
@@ -1,5 +1,33 @@
+fn fibonacci_computation(generations : i32, previous : i32, pprevious : i32) -> i32
+{
+    if generations < 0
+    {
+        return previous;
+    }
+
+    return fibonacci_computation(generations - 1, previous + pprevious, previous);
+}
+
+
+fn fibonacci(generations : i32) -> i32
+{
+    let mut result;
+
+    if generations == 0 || generations == 1
+    {
+        let result = generations;
+    }
+
+    else
+    {
+        let result = {
+            fibonacci_computation(generations - 2, 1, 0)
+        };
+    }
+
+    return result;
+}
+
 fn main() {
-    let a = 2;
-    let b = 3;
-    let mut r;
+    print!(fibonacci(12));
 }
